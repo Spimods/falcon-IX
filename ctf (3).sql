32a03c7daa5a34c15e1056bc212b88dce363dc0d
Spimods <132432388+Spimods@users.noreply.github.com>
2024-01-30 08:30:49 +0100

    ctf update

--- ctf (3).sql
+++ ctf (3).sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : mar. 30 jan. 2024 à 06:35
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `ctf`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `ctfuser`
---
 
 CREATE TABLE `ctfuser` (
   `id` int(11) NOT NULL,
@@ -41,18 +16,8 @@ CREATE TABLE `ctfuser` (
   `No` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `ctfuser`
---
-
 INSERT INTO `ctfuser` (`id`, `nom`, `errors`, `keyword`, `finish`, `time_finish`, `ip`, `cookie`, `n_modele`, `n_connect`, `No`) VALUES
 (3, 'grgoire', NULL, NULL, NULL, NULL, '::1', '65af66b18ce07', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `prog`
---
 
 CREATE TABLE `prog` (
   `id` int(11) NOT NULL,
@@ -66,18 +31,8 @@ CREATE TABLE `prog` (
   `cookie` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `prog`
---
-
 INSERT INTO `prog` (`id`, `nom`, `flag1`, `flag2`, `flag3`, `time_flag_1`, `time_flag_2`, `time_flag_3`, `cookie`) VALUES
 (3, 'grgoire', 0, 0, 0, '0', '0', '0', '65af66b18ce07');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `python`
---
 
 CREATE TABLE `python` (
   `id` int(11) NOT NULL,
@@ -91,19 +46,9 @@ CREATE TABLE `python` (
   `cookie` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `python`
---
-
 INSERT INTO `python` (`id`, `nom`, `flag1`, `flag2`, `flag3`, `time_flag_1`, `time_flag_2`, `time_flag_3`, `cookie`) VALUES
 (3, 'grgoire', '1', '1', '1', '0h 1min 37sec', '0h 2min 43sec', '0h 0min 9sec', '65af66b18ce07'),
 (4, 'TEST', '1', '1', '1', '0h 0min 90sec', '0h 0min 58sec', '0h 0min 42sec', '65af66b18ce08');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `rsociaux`
---
 
 CREATE TABLE `rsociaux` (
   `id` int(11) NOT NULL,
@@ -117,12 +62,6 @@ CREATE TABLE `rsociaux` (
   `cookie` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `score`
---
-
 CREATE TABLE `score` (
   `nom` varchar(50) NOT NULL,
   `note` varchar(20) NOT NULL,
@@ -133,14 +72,9 @@ CREATE TABLE `score` (
   `cookie` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `score`
---
-
 INSERT INTO `score` (`nom`, `note`, `timetotal`, `caracteretotal`, `lignetotal`, `codecomplet`, `cookie`) VALUES
 ('grgoire', '10', '0-0-4', '0', '0', 'print(\"pair\" if int(input(\"Entrez un nombre : \")) % 2 == 0 else \"impair\")', '65a666e605f6e'),
 ('grgoire', '10', '0-0-5', '0', '0', 'nombre1 = int(input(\"Entrez un nombre : \"))\nnombre2 = int(input(\"Entrez un nombre : \"))\nnombre3 = int(input(\"Entrez un nombre : \"))\n\nprint(max(nombre1, nombre2, nombre3))', '65a666e605f6e'),
-('grgoire', '10', '0-0-4', '0', '0', '# Demander à l\'utilisateur de saisir les trois notes\nnote1 = float(input(\"Entrez la première note : \"))\nnote2 = float(input(\"Entrez la deuxième note : \"))\nnote3 = float(input(\"Entrez la troisième note : \"))\n\nprint((note1 + note2 + note3)/3)', '65a666e605f6e'),
 ('grgoire', '10', '0-3-40', '0', '0', '# Demander à l\'utilisateur de saisir les trois notes\nnote1 = float(input(\"Entrez la première note : \"))\nnote2 = float(input(\"Entrez la deuxième note : \"))\nnote3 = float(input(\"Entrez la troisième note : \"))\n\nprint((note1 + note2 + note3)/3)', '65a666e605f6e'),
 ('grgoire', '10', '0-4-11', '0', '0', '# Demander à l\'utilisateur de saisir les trois notes\nnote1 = float(input(\"Entrez la première note : \"))\nnote2 = float(input(\"Entrez la deuxième note : \"))\nnote3 = float(input(\"Entrez la troisième note : \"))\n\nprint((note1 + note2 + note3)/3)', '65a666e605f6e'),
 ('grgoire', '10', '0-6-53', '0', '0', '# Demander à l\'utilisateur de saisir les trois notes\nnote1 = float(input(\"Entrez la première note : \"))\nnote2 = float(input(\"Entrez la deuxième note : \"))\nnote3 = float(input(\"Entrez la troisième note : \"))\n\nprint((note1 + note2 + note3)/3)', '65a666e605f6e'),
@@ -347,11 +281,6 @@ INSERT INTO `score` (`nom`, `note`, `timetotal`, `caracteretotal`, `lignetotal`,
 ('grgoire', '10', '0-0-3', '0', '0', 'print(\'\')', '65af66b18ce07'),
 ('grgoire', '8.97', '0-0-3', '73', '6', 'print(\'\')\nprint(\'\')\nfor i in range(23):\n    print(f\"{i} est e\")\n    \nprint(\'\')', '65af66b18ce07');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `timepython`
---
 
 CREATE TABLE `timepython` (
   `id` int(11) NOT NULL,
@@ -377,18 +306,8 @@ CREATE TABLE `timepython` (
   `key9` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `timepython`
---
-
 INSERT INTO `timepython` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `time7`, `time8`, `time9`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`, `key7`, `key8`, `key9`) VALUES
 (0, 'grgoire', '65af66b18ce07', '0h0min37sec', '0h0min25sec', '0h0min35sec', '0h1min1sec', '0h1min25sec', '0h0min17sec', '0h0min3sec', '0h0min3sec', '0h0min3sec', 1, 1, 1, 1, 1, 1, 1, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `triche`
---
 
 CREATE TABLE `triche` (
   `id` int(11) NOT NULL,
@@ -397,19 +316,9 @@ CREATE TABLE `triche` (
   `cookie` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `triche`
---
-
 INSERT INTO `triche` (`id`, `nom`, `etape`, `cookie`) VALUES
 (1, 'grgoire', 'etape7', '65af66b18ce07'),
 (2, 'grgoire', 'etape8', '65af66b18ce07');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `valeur python`
---
 
 CREATE TABLE `valeur python` (
   `valeur_1` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -421,10 +330,6 @@ CREATE TABLE `valeur python` (
   `nom` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Déchargement des données de la table `valeur python`
---
-
 INSERT INTO `valeur python` (`valeur_1`, `result_1`, `valeur_2`, `result_2`, `valeur_3`, `result_3`, `nom`) VALUES
 ('98', 'pair', '121', 'impair', '18', 'pair', 'partie1'),
 ('12 23 22', '23', '58 21 32', '58', '23 34 97', '97', 'partie2'),
@@ -432,87 +337,40 @@ INSERT INTO `valeur python` (`valeur_1`, `result_1`, `valeur_2`, `result_2`, `va
 ('12 [11,12,23,27,1]', 'True', '23 [248,234,19,24,91]', 'False', '3 [1,2,3]', 'True', 'partie4'),
 ('12', '2 4 6 8 10', '23', '2 4 6 8 10 12 14 16 18 20 22', '3', '2', 'partie5');
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `ctfuser`
---
 ALTER TABLE `ctfuser`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `prog`
---
 ALTER TABLE `prog`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `python`
---
 ALTER TABLE `python`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `rsociaux`
---
 ALTER TABLE `rsociaux`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `timepython`
---
 ALTER TABLE `timepython`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `triche`
---
 ALTER TABLE `triche`
   ADD PRIMARY KEY (`id`);
 
---
--- Index pour la table `valeur python`
---
 ALTER TABLE `valeur python`
   ADD PRIMARY KEY (`nom`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `ctfuser`
---
 ALTER TABLE `ctfuser`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT pour la table `prog`
---
 ALTER TABLE `prog`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT pour la table `python`
---
 ALTER TABLE `python`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT pour la table `rsociaux`
---
 ALTER TABLE `rsociaux`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT pour la table `triche`
---
 ALTER TABLE `triche`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;

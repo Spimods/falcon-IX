e4c000a6b256fa4a51ed4fb0b2ecab1dadac1a14
Spimods <132432388+Spimods@users.noreply.github.com>
2024-02-08 19:05:53 +0100

    Update etape1.php

--- python/etape1.php
+++ python/etape1.php
@@ -52,16 +52,16 @@ if (isset($_SESSION['ctfcookies'])) {
         <div class="area">
             <div class="wire"></div>
             <div class="fixture">
-              <div class="strip"></div>
-              <div class="strip"></div>
-              <div class="strip"></div>
+            <div class="strip"></div>
+            <div class="strip"></div>
+            <div class="strip"></div>
             </div>
             <div class="bulb">
-              <div class="zig"></div>
-              <div class="zig"></div>
-              <div class="zig"></div>
+            <div class="zig"></div>
+            <div class="zig"></div>
+            <div class="zig"></div>
             </div>
-          </div>    </div>
+        </div>    </div>
     
     
     <div class="menu" id="menu">
@@ -137,9 +137,7 @@ if (isset($_SESSION['ctfcookies'])) {
 </div>
 
 <div id="editor-container">
-<button id="restart" onclick="restart()"><svg xmlns="http://www.w3.org/2000/svg" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/> <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/> </svg></button>
-
-    <div id="editor"># Demander à l'utilisateur de saisir un nombre
+<button id="restart" onclick="restart()"><svg xmlns="http://www.w3.org/2000/svg" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/> <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/> </svg></button>    <div id="editor"># Demander à l'utilisateur de saisir un nombre
 nombre = int(input("Entrez un nombre : "))
 </div>
         <div id="tooltip">

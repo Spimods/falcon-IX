00010c34d0307494e0f5332f3b164db611432c5e
Spimods <132432388+Spimods@users.noreply.github.com>
2024-03-04 12:00:07 +0100

    Update ctf.sql

--- sql/ctf.sql
+++ sql/ctf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 16 fév. 2024 à 15:06
+-- Généré le : lun. 04 mars 2024 à 11:59
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -46,7 +46,7 @@ CREATE TABLE `ctfuser` (
 --
 
 INSERT INTO `ctfuser` (`id`, `nom`, `errors`, `keyword`, `finish`, `time_finish`, `ip`, `cookie`, `n_modele`, `n_connect`, `No`) VALUES
-(1, 'grgoire', NULL, NULL, NULL, NULL, '::1', '65cbc080d4af7', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36', NULL, NULL);
+(1, 'd', NULL, NULL, 1, NULL, '::1', '65e39077d3b85', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -71,7 +71,7 @@ CREATE TABLE `prog` (
 --
 
 INSERT INTO `prog` (`id`, `nom`, `flag1`, `flag2`, `flag3`, `time_flag_1`, `time_flag_2`, `time_flag_3`, `cookie`) VALUES
-(1, 'bh', 1, 1, 0, '0h 0min 43sec', '12h 19min 60sec', '0', '65cbc080d4af7');
+(1, 'd', 0, 0, 0, '0', '0', '0', '65e39077d3b85');
 
 -- --------------------------------------------------------
 
@@ -96,7 +96,7 @@ CREATE TABLE `python` (
 --
 
 INSERT INTO `python` (`id`, `nom`, `flag1`, `flag2`, `flag3`, `time_flag_1`, `time_flag_2`, `time_flag_3`, `cookie`) VALUES
-(1, 'bh', '1', 'triche', '0', '20h min sec', '0', '0', '65cbc080d4af7');
+(1, 'd', '0', '0', '0', '0', '0', '0', '65e39077d3b85');
 
 -- --------------------------------------------------------
 
@@ -133,62 +133,25 @@ CREATE TABLE `score` (
   `cookie` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Déchargement des données de la table `score`
+-- Structure de la table `skills`
 --
 
-INSERT INTO `score` (`nom`, `note`, `timetotal`, `caracteretotal`, `lignetotal`, `codecomplet`, `etape`, `cookie`) VALUES
-('bh', '10', '0-4-20', '0', '0', 'nombre1 = int(input(\"Entrez un nombre : \"))\nnombre2 = int(input(\"Entrez un nombre : \"))\nnombre3 = int(input(\"Entrez un nombre : \"))\nprint(max(nombre1, nombre2, nombre3))', 'python', '65cbc080d4af7'),
-('bh', '10', '0-0-39', '0', '0', '# Demander à l\'utilisateur de saisir les trois notes\nnote1 = float(input(\"Entrez la première note : \"))\nnote2 = float(input(\"Entrez la deuxième note : \"))\nnote3 = float(input(\"Entrez la troisième note : \"))\nprint((note1+note2+note3)/3)', 'python', '65cbc080d4af7'),
-('bh', '10', '20-7-31', '0', '0', '# Demander à l\'utilisateur de saisir les trois notes\nnote1 = float(input(\"Entrez la première note : \"))\nnote2 = float(input(\"Entrez la deuxième note : \"))\nnote3 = float(input(\"Entrez la troisième note : \"))\nprint((note1+note2+note3)/3)', 'python', '65cbc080d4af7'),
-('bh', '10', '0-1-9', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-33', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-44', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-32', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-19', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-42', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-25', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '21-16-30', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-34-6', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-40-16', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-40-48', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-41-7', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-41-14', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-41-17', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-41-55', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-42-2', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-42-4', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-42-4', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '6', '0-9-20', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '9-47-33', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-8-56', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-8-59', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '10-26-0', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '9', '0-3-25', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '9', '0-5-38', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '13-5-44', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '13-6-19', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '13-11-16', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '0', '0-18-0', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-11', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '10', '0-0-20', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '10', '0-0-4', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-3', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-9', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-3', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-2', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-2', '', '', '', 'progHTML', '65cbc080d4af7'),
-('bh', '0', '54-24-41', '', '', '', 'progSQL', '65cbc080d4af7'),
-('bh', '10', '0-0-4', '', '', '', 'progSQL', '65cbc080d4af7'),
-('bh', '10', '0-0-4', '', '', '', 'progSQL', '65cbc080d4af7'),
-('bh', '0', '12-17-16', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-43', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-3', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-3', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-5', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '0', '12-18-17', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '0', '12-18-38', '', '', '', 'progCSS', '65cbc080d4af7'),
-('bh', '10', '0-0-29', '', '', '', 'progCSS', '65cbc080d4af7');
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `skills` varchar(20) NOT NULL,
+  `cookie` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `skills`
+--
+
+INSERT INTO `skills` (`id`, `nom`, `skills`, `cookie`) VALUES
+(1, 'd', '1411231313', '65e39077d3b85');
 
 -- --------------------------------------------------------
 
@@ -197,7 +160,6 @@ INSERT INTO `score` (`nom`, `note`, `timetotal`, `caracteretotal`, `lignetotal`,
 --
 
 CREATE TABLE `timeprog` (
-  `id` int(11) NOT NULL,
   `nom` varchar(20) NOT NULL,
   `cookie` varchar(40) NOT NULL,
   `time1` varchar(30) DEFAULT NULL,
@@ -211,15 +173,16 @@ CREATE TABLE `timeprog` (
   `key3` int(11) DEFAULT NULL,
   `key4` int(11) DEFAULT NULL,
   `key5` int(11) DEFAULT NULL,
-  `key6` int(11) DEFAULT NULL
+  `key6` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `timeprog`
 --
 
-INSERT INTO `timeprog` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`) VALUES
-(0, 'bh', '65cbc080d4af7', '0h1min9sec', '12h17min16sec', '0h0min44sec', '0h0min32sec', '0h0min19sec', '0h0min42sec', 1, 1, 1, 1, 1, 1);
+INSERT INTO `timeprog` (`nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`, `id`) VALUES
+('d', '65e39077d3b85', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -228,7 +191,6 @@ INSERT INTO `timeprog` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `time4
 --
 
 CREATE TABLE `timeproghtml` (
-  `id` int(11) NOT NULL,
   `nom` varchar(20) NOT NULL,
   `cookie` varchar(40) NOT NULL,
   `time1` varchar(30) DEFAULT NULL,
@@ -242,15 +204,16 @@ CREATE TABLE `timeproghtml` (
   `key3` int(11) DEFAULT NULL,
   `key4` int(11) DEFAULT NULL,
   `key5` int(11) DEFAULT NULL,
-  `key6` int(11) DEFAULT NULL
+  `key6` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `timeproghtml`
 --
 
-INSERT INTO `timeproghtml` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`) VALUES
-(0, 'bh', '65cbc080d4af7', '0h0min11sec', '0h0min20sec', '0h0min4sec', '0h0min3sec', '0h0min3sec', '0h0min2sec', 1, 1, 1, 1, 1, 1);
+INSERT INTO `timeproghtml` (`nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`, `id`) VALUES
+('d', '65e39077d3b85', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -259,7 +222,6 @@ INSERT INTO `timeproghtml` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `t
 --
 
 CREATE TABLE `timeprogsql` (
-  `id` int(11) NOT NULL,
   `nom` varchar(20) NOT NULL,
   `cookie` varchar(40) NOT NULL,
   `time1` varchar(30) DEFAULT NULL,
@@ -273,15 +235,16 @@ CREATE TABLE `timeprogsql` (
   `key3` int(11) DEFAULT NULL,
   `key4` int(11) DEFAULT NULL,
   `key5` int(11) DEFAULT NULL,
-  `key6` int(11) DEFAULT NULL
+  `key6` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `timeprogsql`
 --
 
-INSERT INTO `timeprogsql` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`) VALUES
-(0, 'bh', '65cbc080d4af7', '0h0min4sec', '0h0min43sec', '0h0min3sec', '0h0min3sec', '12h18min38sec', '0h0min29sec', 1, 1, 1, 1, 1, 1);
+INSERT INTO `timeprogsql` (`nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`, `id`) VALUES
+('d', '65e39077d3b85', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -290,7 +253,6 @@ INSERT INTO `timeprogsql` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `ti
 --
 
 CREATE TABLE `timepython` (
-  `id` int(11) NOT NULL,
   `nom` varchar(20) NOT NULL,
   `cookie` varchar(40) NOT NULL,
   `time1` varchar(30) DEFAULT NULL,
@@ -310,15 +272,16 @@ CREATE TABLE `timepython` (
   `key6` int(11) DEFAULT NULL,
   `key7` int(11) DEFAULT NULL,
   `key8` int(11) DEFAULT NULL,
-  `key9` int(11) DEFAULT NULL
+  `key9` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `timepython`
 --
 
-INSERT INTO `timepython` (`id`, `nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `time7`, `time8`, `time9`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`, `key7`, `key8`, `key9`) VALUES
-(0, 'bh', '65cbc080d4af7', '13 19 19 05', '0h4min20sec', '20h7min31sec', '13 20 07 38', '13 20 07 58', NULL, NULL, NULL, NULL, 1, 1, 1, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `timepython` (`nom`, `cookie`, `time1`, `time2`, `time3`, `time4`, `time5`, `time6`, `time7`, `time8`, `time9`, `key1`, `key2`, `key3`, `key4`, `key5`, `key6`, `key7`, `key8`, `key9`, `id`) VALUES
+('d', '65e39077d3b85', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -332,69 +295,6 @@ CREATE TABLE `triche` (
   `etape` varchar(20) NOT NULL,
   `cookie` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `triche`
---
-
-INSERT INTO `triche` (`id`, `nom`, `etape`, `cookie`) VALUES
-(1, 'bh', 'etape1', '65cbc080d4af7'),
-(2, 'bh', 'etape4', '65cbc080d4af7'),
-(3, 'bh', 'etape3', '65cbc080d4af7'),
-(4, 'bh', 'etape3', '65cbc080d4af7'),
-(5, 'bh', 'etape3', '65cbc080d4af7'),
-(6, 'bh', 'etape3', '65cbc080d4af7'),
-(7, 'bh', 'etape3', '65cbc080d4af7'),
-(8, 'bh', 'etape3', '65cbc080d4af7'),
-(9, 'bh', 'etape3', '65cbc080d4af7'),
-(10, 'bh', 'etape3', '65cbc080d4af7'),
-(11, 'bh', 'etape3', '65cbc080d4af7'),
-(12, 'bh', 'etape3', '65cbc080d4af7'),
-(13, 'bh', 'etape3', '65cbc080d4af7'),
-(14, 'bh', 'etape3', '65cbc080d4af7'),
-(15, 'bh', 'etape3', '65cbc080d4af7'),
-(16, 'bh', 'etape3', '65cbc080d4af7'),
-(17, 'bh', 'etape3', '65cbc080d4af7'),
-(18, 'bh', 'etape3', '65cbc080d4af7'),
-(19, 'bh', 'etape3', '65cbc080d4af7'),
-(20, 'bh', 'etape3', '65cbc080d4af7'),
-(21, 'bh', 'etape3', '65cbc080d4af7'),
-(22, 'bh', 'etape3', '65cbc080d4af7'),
-(23, 'bh', 'etape3', '65cbc080d4af7'),
-(24, 'bh', 'etape3', '65cbc080d4af7'),
-(25, 'bh', 'etape4', '65cbc080d4af7'),
-(26, 'bh', 'etape4', '65cbc080d4af7'),
-(27, 'bh', 'etape4', '65cbc080d4af7'),
-(28, 'bh', 'etape4', '65cbc080d4af7'),
-(29, 'bh', 'etape4', '65cbc080d4af7'),
-(30, 'bh', 'etape4', '65cbc080d4af7'),
-(31, 'bh', 'etape4', '65cbc080d4af7'),
-(32, 'bh', 'etape4', '65cbc080d4af7'),
-(33, 'bh', 'etape4', '65cbc080d4af7'),
-(34, 'bh', 'etape4', '65cbc080d4af7'),
-(35, 'bh', 'etape4', '65cbc080d4af7'),
-(36, 'bh', 'etape4', '65cbc080d4af7'),
-(37, 'bh', 'etape4', '65cbc080d4af7'),
-(38, 'bh', 'etape4', '65cbc080d4af7'),
-(39, 'bh', 'etape4', '65cbc080d4af7'),
-(40, 'bh', 'etape4.php?name=hell', '65cbc080d4af7'),
-(41, 'bh', 'etape4.php?name=hell', '65cbc080d4af7'),
-(42, 'bh', 'etape4.php?name=hell', '65cbc080d4af7'),
-(43, 'bh', 'etape4', '65cbc080d4af7'),
-(44, 'bh', 'etape4', '65cbc080d4af7'),
-(45, 'bh', 'etape6', '65cbc080d4af7'),
-(46, 'bh', 'etape6', '65cbc080d4af7'),
-(47, 'bh', 'etape6', '65cbc080d4af7'),
-(48, 'bh', 'etape5', '65cbc080d4af7'),
-(49, 'bh', 'etape5', '65cbc080d4af7'),
-(50, 'bh', 'etape5', '65cbc080d4af7'),
-(51, 'bh', 'etape1', '65cbc080d4af7'),
-(52, 'bh', 'etape1', '65cbc080d4af7'),
-(53, 'bh', 'etape1', '65cbc080d4af7'),
-(54, 'bh', 'etape1', '65cbc080d4af7'),
-(55, 'bh', 'etape1', '65cbc080d4af7'),
-(56, 'bh', 'etape1', '65cbc080d4af7'),
-(57, 'bh', 'etape1', '65cbc080d4af7');
 
 -- --------------------------------------------------------
 
@@ -453,6 +353,12 @@ ALTER TABLE `python`
 -- Index pour la table `rsociaux`
 --
 ALTER TABLE `rsociaux`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `skills`
+--
+ALTER TABLE `skills`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -520,10 +426,40 @@ ALTER TABLE `rsociaux`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT pour la table `skills`
+--
+ALTER TABLE `skills`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `timeprog`
+--
+ALTER TABLE `timeprog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `timeproghtml`
+--
+ALTER TABLE `timeproghtml`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `timeprogsql`
+--
+ALTER TABLE `timeprogsql`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `timepython`
+--
+ALTER TABLE `timepython`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT pour la table `triche`
 --
 ALTER TABLE `triche`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
